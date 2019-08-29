796b73b27a7319726bce8da864c7c327c8a50f9d
Emil <rrotatew@gmail.com>
2019-08-29 20:25:56 +0300

    2

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: 'app/',
+        },
+        notify: false,
+
+    });
+});
+
+
+
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+
+});
+
+
+
+
+gulp.task('default', ['watch']);
